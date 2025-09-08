00e91c28add0814b2009c627f82fc518e92fa3ba
IcosagonZ <rohanraj3870z@gmail.com>
2025-09-08 18:03:59 +0530

    Added game logic (to display letters, and check if winning or losing)

--- lib/main.dart
+++ lib/main.dart
@@ -38,52 +38,26 @@ class Page_Main extends StatefulWidget
 class _Page_MainState extends State<Page_Main>
 {
   // Game variables
-  String game_word_display = "h e l l o";
+  var game_won = false;
+  var game_lost = false;
+
+  String game_word_display = "_____";
   String game_word = "hello";
 
-  String game_used = "";
+  String game_letters_correct = ""; // Controls visibility of text on game word display
+  String game_letters_used = ""; // Controls visibility of used letters
 
   var words;
   var words_keys_list;
   var words_total = 0;
 
-  // Visibility variables
-  int person_state = 0;
+  int game_tries_max = 7;
+  int game_tries_incorrect = 0; // Controls visibility of man
 
   // Keyboard generation variables
   String row0  = "qwertyuiop";
   String row1 = "asdfghjkl";
   String row2 = "zxcvbnm";
-
-  // Keyboard button generation variable
-  List <Widget> keyboardGenerateButtons(String row)
-  {
-    return List<Widget>.generate(row.length, (index)
-    {
-      return Expanded
-      (
-        child: TextButton
-        (
-          onPressed: !game_used.contains(row[index]) ? ()
-          {
-            print("Pressed ${row[index]}");
-          } : null,
-          child: Text
-          (
-            row[index],
-            style: TextStyle
-            (
-              fontSize: 20,
-            )
-          ),
-          style: ElevatedButton.styleFrom
-          (
-            minimumSize: Size(double.infinity, double.infinity),
-          ),
-        )
-      );
-    });
-  }
 
   // Random number finder
   int randomNumber(int min, int max)
@@ -122,13 +96,110 @@ class _Page_MainState extends State<Page_Main>
     }
   }
 
+  void word_display_update()
+  {
+    game_word_display="";
+    for(var letter in game_word.split(""))
+    {
+      if(game_letters_correct.contains(letter))
+      {
+        game_word_display+=letter;
+      }
+      else
+      {
+        game_word_display+="_";
+      }
+    }
+  }
+
+  void word_pressed(String letter)
+  {
+    if(game_word.contains(letter))
+    {
+      game_letters_used+=letter;
+      game_letters_correct+=letter;
+    }
+    else
+    {
+      game_letters_used+=letter;
+      game_tries_incorrect++;
+      if(game_tries_incorrect>game_tries_max)
+      {
+        game_lost=true;
+        print("GAME: Lost");
+      }
+    }
+  }
+
+  void word_check_win()
+  {
+    if(game_word==game_word_display)
+    {
+      game_won=true;
+      print("GAME: Win");
+    }
+  }
+
   void ui_restart_clicked()
   {
     setState(()
     {
-      game_word = word_retrive();
+      game_word = word_retrive(); // Pick word randomly
+
+      // Reset game variables
+      game_won = false;
+      game_lost = false;
+
+      game_tries_incorrect = 0;
+
+      game_letters_correct = "";
+      game_letters_used = "";
+
+      word_display_update();
     }
     );
+  }
+
+  void ui_keyboard_clicked(String letter)
+  {
+    setState(()
+    {
+      word_pressed(letter);
+      word_display_update();
+      word_check_win();
+    }
+    );
+  }
+
+  // Keyboard button generation variable
+  List <Widget> keyboardGenerateButtons(String row)
+  {
+    return List<Widget>.generate(row.length, (index)
+    {
+      return Expanded
+      (
+        child: TextButton
+        (
+          onPressed: !game_letters_used.contains(row[index]) ? ()
+          {
+            ui_keyboard_clicked(row[index]);
+            word_display_update();
+          } : null,
+          child: Text
+          (
+            row[index],
+            style: TextStyle
+            (
+              fontSize: 20,
+            )
+          ),
+          style: ElevatedButton.styleFrom
+          (
+            minimumSize: Size(double.infinity, double.infinity),
+          ),
+        )
+      );
+    });
   }
 
   @override
@@ -179,37 +250,37 @@ class _Page_MainState extends State<Page_Main>
                     Image.asset("assets/person/person_0.png"),
                     Visibility
                     (
-                      visible: person_state>=1,
+                      visible: game_tries_incorrect>=1,
                       child: Image.asset("assets/person/person_1.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=2,
+                      visible: game_tries_incorrect>=2,
                       child: Image.asset("assets/person/person_2.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=3,
+                      visible: game_tries_incorrect>=3,
                       child: Image.asset("assets/person/person_3.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=4,
+                      visible: game_tries_incorrect>=4,
                       child: Image.asset("assets/person/person_4.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=5,
+                      visible: game_tries_incorrect>=5,
                       child: Image.asset("assets/person/person_5.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=6,
+                      visible: game_tries_incorrect>=6,
                       child: Image.asset("assets/person/person_6.png"),
                     ),
                     Visibility
                     (
-                      visible: person_state>=7,
+                      visible: game_tries_incorrect>=7,
                       child: Image.asset("assets/person/person_7.png"),
                     ),
                   ]
@@ -219,7 +290,7 @@ class _Page_MainState extends State<Page_Main>
             // Text entry
             Text
             (
-              game_word,
+              game_word_display,
               style: TextStyle
               (
                 fontFamily: "monofett",
